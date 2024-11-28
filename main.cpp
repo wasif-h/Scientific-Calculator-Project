fb072d9c9c3773f0cbcd6d8bba6c127db608c855
Wasif Hossain <wasif.hx@gmail.com>
2024-11-28 14:44:18 +0600

    Intro added to main cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
-// Calculator Project
+                                       
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// <<<---------  main function   --------->>> 
+int main()
+{
+
+    cout << "---- Basic Calculator ----\n\n";
+    cout <<"\t1. Addition" << endl;
+    cout <<"\t2. Subtraction" << endl;
+    cout <<"\t3. Multiply" << endl;
+    cout <<"\t4. Division" << endl;
+    int opt;
+    cout << "\n\nEnter Options to perform -->> ";
+    cin >> opt;
+
+    
+
+
+    
+
+
+
+    return 0;
+}
