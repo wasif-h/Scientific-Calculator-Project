fdb112f42c76fccbed6cb1d85d8dfc1f3e761d2b
Wasif Hossain <wasif.hx@gmail.com>
2024-11-28 14:52:26 +0600

    adding + feature

--- main.cpp
+++ main.cpp
@@ -1,28 +1,46 @@
-                                       
+
 #include <bits/stdc++.h>
 using namespace std;
 
+int addition(int a, int b)
+{
+    return a + b;
+}
 
-
-// <<<---------  main function   --------->>> 
+// <<<---------  main function   --------->>>
 int main()
 {
 
     cout << "---- Basic Calculator ----\n\n";
-    cout <<"\t1. Addition" << endl;
-    cout <<"\t2. Subtraction" << endl;
-    cout <<"\t3. Multiply" << endl;
-    cout <<"\t4. Division" << endl;
+    
+    // feature options
+    cout << "\t1. Addition" << endl;
+    cout << "\t2. Subtraction" << endl;
+    cout << "\t3. Multiply" << endl;
+    cout << "\t4. Division" << endl;
     int opt;
     cout << "\n\nEnter Options to perform -->> ";
     cin >> opt;
 
-    
+    // adding feature to the switch case to perform the specific funciton according to the users input
 
+    int a, b;
+    cout << "Enter first value : ";
+    cin >> a;
+    cout << "Enter Second value : ";
+    cin >> b;
 
-    
+    cout << "a = " << a << "\nb = " << b << endl;
 
+    switch (opt)
+    {
+    case 1:
+        cout << "a + b = " << addition(a,b);
+            break;
 
+    default:
+        break;
+    }
 
     return 0;
 }
